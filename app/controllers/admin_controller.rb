d08120ece0e5fd20fa486be8c7cd6a0eedce95bc
deannanicdao <deanna.nicdao@gmail.com>
2020-11-10 21:14:48 +1000

    Removed user email from index items page. Need to style these into cards.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,28 @@
 class AdminController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_item, only: [:show, :edit, :update, :destroy]
+    before_action :check_roles, except: [:index, :show, :destroy]
+
     def users_listings
         @items = Item.all
     end
 
-    
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def check_roles
+      return if user_signed_in? && current_user.has_role?(:admin)
+      return if user_signed_in? && current_user.has_role?(:user)
+      return if user_signed_in? && @item && @item.user == current_user 
+      flash[:alert] = "You are not authorized to access that page."
+      redirect_to root_path
+    end
+
+    # Only allow a list of trusted parameters through.
+    def item_params
+      params.require(:item).permit(:title, :description, :price, :category, :availability, :user_id, :image)
+    end
 end
