bdda11690189eccef21d501a87e8b362a2b3d636
deannanicdao <deanna.nicdao@gmail.com>
2020-11-10 22:27:28 +1000

    The galley/your gallery/admin page now has the correct authorisation. Only missing the purchase button which should be shown for items not belonging to the current user.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,6 +7,14 @@ class AdminController < ApplicationController
         @items = Item.all
     end
 
+    def destroy
+        @item.destroy
+        respond_to do |format|
+          format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+    end
+
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_item
