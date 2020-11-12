b6e2c238f73bafbba8c0ee5b1d03a0f104e0848f
deannanicdao <deanna.nicdao@gmail.com>
2020-11-12 13:20:03 +1000

    Added first name, last name, dob and password complexity to user model and form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :first_name, :last_name, :dob
   rolify
   after_create :assign_default_role
   # Include default devise modules. Others available are:
@@ -10,5 +11,21 @@ class User < ApplicationRecord
 
   def assign_default_role
     self.add_role(:user)
+  end
+
+  # Cloudinary Profile Image
+  has_one_attached :profile_image
+
+  # Sign-Up Validations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :dob, presence: true
+  validate :password_complexity
+  
+  # Password Security
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,20}$/
+
+    errors.add :password, 'Password complexity requirement not met.'
   end
 end
